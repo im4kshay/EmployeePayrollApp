dbd2d19e046f8423d336622a06b734a1159fb373
im4kshay <akshaysayre7@gmail.com>
2022-05-24 09:39:32 +0530

    [Akshay]Added: Event listener on document load

--- js/EmpPayrollRegistration.js
+++ js/EmpPayrollRegistration.js
@@ -1,6 +1,24 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
+window.addEventListener('DOMContentLoaded',(event) =>{
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-})
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+    
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input',function(){
+      if(name.value.length == 0){
+        textError.textContent = "";
+        return;
+      }
+      try{
+        (new EmployeePayrollData()).name = name.value;
+      }
+      catch(ex){
+        textError.textContent = ex;
+      }
+    }); 
+    });
